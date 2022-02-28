29544ed76c6dd4994fed670f70c825d7493f99bb
Valexandrin <alex-vadim@mail.ru>
2022-02-28 12:51:00 +0300

    Bootstrap framework was added

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title = 'Прогноз погоды'
+    title = 'Новости Python'
     weather = weather_by_city('Kazan')    
     return render_template('index.html', page_title=title, weather=weather)
 
