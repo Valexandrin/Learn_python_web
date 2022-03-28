335656e338023862380e3e03bdbfae24807eeb68
Valexandrin <alex-vadim@mail.ru>
2022-03-28 20:57:15 +0300

    Flask-migrations and registration user added

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField(
@@ -14,6 +15,29 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField(
         'Remember me', default=True,
         render_kw={"class": "form-check-input"},
+    )
+    submit = SubmitField(
+        'Send!',
+        render_kw={"class":"btn btn-primary"},
+    )
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        'Name', validators=[DataRequired()], 
+        render_kw={"class": "form-control"},
+    )
+    email = StringField(
+        'email', validators=[DataRequired(), Email()], 
+        render_kw={"class": "form-control"},
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired()],
+        render_kw={"class": "form-control"},
+    )
+    password2 = PasswordField(
+        'Confirm password', validators=[DataRequired(), EqualTo('password')],
+        render_kw={"class": "form-control"},
     )
     submit = SubmitField(
         'Send!',
